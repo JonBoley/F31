b55c39d4fd9b5eefbb84eff3545880d19827998e
Jon2980 <Jon2980@daba21ff-bcea-44df-ba80-775e93682a96>
2012-01-11 17:26:42 +0000

    minor updates to handle alternative ways of running the script

--- Vowel_STMP/Mfiles/batch_run.m
+++ Vowel_STMP/Mfiles/batch_run.m
@@ -2,7 +2,9 @@
 setup_Vowel_STMP;
 
 %% Run all units for a given experiment
-date = '062711';
+if ~exist('date','var')
+    date = '041811';
+end
 switch date
     case '041805'   % 04/18/05 fibers: (in noise, normal)
         % 1.01, 1.04, 1.10, 2.04, 3.03, 4.06, 4.09, 4.11
@@ -29,12 +31,13 @@ switch date
     case '121404'   % 12/14/04 fibers: (in quiet, impaired)
         % 1.03, 1.07, 1.08, 2.03, 2.04, 2.05, 2.07, 2.09, 7.01, 7.02, 7.05, 7.06,
         % 7.09, 7.10, 8.01, 9.01, 9.03, 9.05, 9.08, 9.10, 9.15, 9.16, 11.02, 13.01,
-        % 13.02, 13.04    
-        
-    case '011811'   % 01/18/11 fiber: (in noise, normal but exposed)
+        % 13.02, 13.04
+
+    case '011811'   % 01/18/11 fiber: (in white noise, normal but exposed)
         % 2.01, 2.02, 2.06, 2.08, 3.02, 3.08, 3.10, 3.11
         unitNums = {2.01, 2.02, 2.06, 2.08, 3.02, 3.08, 3.10, 3.11};
         colors = repmat({'k'},size(unitNums));
+    
     case '041811'   % JB-2011_04_18-AN_Normal
         % 1.03, 2.01, 2.02, 2.07, 3.03, 3.04
         unitNums = {1.03, 2.01, 2.02, 2.07, 3.03, 3.04};
@@ -52,7 +55,7 @@ switch date
         % 1.02, 1.03, 1.05, 1.06, 1.07, 1.08, 1.09, 1.10
         unitNums = {1.02, 1.03, 1.05, 1.06, 1.07, 1.08, 1.09, 1.10};
         colors = repmat({'k'},size(unitNums));
-        
+
     case '062311'   % JB-2011_06_23-Chin1119_AN_500OBN
         % 1.01, 1.02, 1.09, 1.11, 1.13, 1.14
         unitNums = {1.01, 1.02, 1.09, 1.11, 1.13, 1.14};
@@ -74,7 +77,7 @@ switch date
         % 3.09
         unitNums = {1.01, 1.02, 1.05, 1.06, 1.07, 1.08, 1.10, 1.13, 3.02, 3.05, 3.06, 3.07, 3.09};
         colors = repmat({'r'},size(unitNums));
-    
+
     case '100611'  % JB-2011_10_06-Chin1144_AN_normal
         % 1.02, 1.10, 3.06
         unitNums = {1.02, 3.06};
@@ -87,8 +90,8 @@ switch date
         % 1.03, 2.04, 3.01, 3.02, 3.03
         unitNums = {1.03, 2.04, 3.01, 3.02, 3.03};
         colors = repmat({'k'},size(unitNums)); %normal=black
-        
-    % ultra-wide STMP (F1 & F2)    
+
+        % ultra-wide STMP (F1 & F2)
     case '112911'   % JB-2011_11_29-Chin1146_AN_normal
         % 1.09, 3.03, 4.04, 4.12, 4.13, 4.14, 4.16
         unitNums = {1.09, 3.03, 4.04, 4.12, 4.13, 4.14, 4.16};
@@ -97,7 +100,7 @@ switch date
         % 1.09, 2.01, 2.05, 2.08, 2.11, 2.12, 2.14, 2.19, 2.23, 2.26, 2.32, 2.33, 2.34
         unitNums = {2.05, 2.08, 2.11, 2.12, 2.14, 2.19, 2.32, 2.34};
         colors = repmat({'k'},size(unitNums));
-        
+
     otherwise
         error('Invalid experiment date');
 end
@@ -112,161 +115,164 @@ dates=repmat({date},size(unitNums));
 %     'r','r','r','r','r'};
 
 %% The loop
-for i=1%1:3%length(unitNums)
+RecalcAll = 0;  % if enabled, this automatically recalculates everything,
+                % but loads previous characteristic delays
+for i=1:length(unitNums)
     date = dates{i};
     disp(sprintf('%d) [%s] Calculating unit %1.2f...',i,date,unitNums{i}));
 
     % Use this for SNR conditions:
-%     [UnitCF(i),UnitThresh(i),UnitQ10(i),...
-%         Rate_failpoint(i),Rate_fail_limit(i),...
-%         ALSR_failpoint(i),ALSR_fail_limit(i),...
-%         Nscc_CD_pos_failpoint(i),Nscc_CD_pos_fail_limit(i),...
-%         Nscc_CD_neg_failpoint(i),Nscc_CD_neg_fail_limit(i),...
-%         Nscc0_pos_failpoint(i),Nscc0_pos_fail_limit(i),...
-%         Nscc0_neg_failpoint(i),Nscc0_neg_fail_limit(i)]=...
-        UnitLook_EHIN_CoincDet2_JB(date,num2str(unitNums{i}));
+    %     [UnitCF(i),UnitThresh(i),UnitQ10(i),...
+    %         Rate_failpoint(i),Rate_fail_limit(i),...
+    %         ALSR_failpoint(i),ALSR_fail_limit(i),...
+    %         Nscc_CD_pos_failpoint(i),Nscc_CD_pos_fail_limit(i),...
+    %         Nscc_CD_neg_failpoint(i),Nscc_CD_neg_fail_limit(i),...
+    %         Nscc0_pos_failpoint(i),Nscc0_pos_fail_limit(i),...
+    %         Nscc0_neg_failpoint(i),Nscc0_neg_fail_limit(i)]=...
+    UnitLook_EHIN_CoincDet2_JB(date,num2str(unitNums{i}),RecalcAll);
 
     % Use this for level conditions:
     %     [UnitCF(i),UnitThresh(i),UnitQ10(i)]=...
     %         UnitLook_EH_CoincDet2_JB(num2str(unitNums{i}));
 
-    pause; 
+    if ~RecalcAll, pause; end
     close all; home;
 end
 
-% Plot Threshold
-figure, subplot(211),
-load normema % load normal thresholds
-semilogx(normt(1,:),normt(2,:),'k'); hold on;
-for i=1:length(UnitCF)
-    semilogx(UnitCF(i),UnitThresh(i),[colors{i} 'x']);
+if exist('UnitCF')
+    % Plot Threshold
+    figure, subplot(211),
+    load normema % load normal thresholds
+    semilogx(normt(1,:),normt(2,:),'k'); hold on;
+    for i=1:length(UnitCF)
+        semilogx(UnitCF(i),UnitThresh(i),[colors{i} 'x']);
+    end
+    hold off;
+    legend('Miller et al. (1997)','This Experiment');
+    ylabel('Threshold (dB SPL)'); xlabel('Best Frequency (kHz)');
+    axis([0.1 20 -10 110]);
+    % Plot Q10
+    subplot(212),
+    QlowM97(:,1)=[.27 10]';
+    QhighM97(:,1)=QlowM97(:,1); % Add lines from Miller et al 1997 Normal data
+    QlowM97(:,2)=10^.2779*QlowM97(:,1).^.4708;
+    QhighM97(:,2)=10^.6474*QlowM97(:,1).^.4708; % 5th and 95th percentiles from Miller et al, 1997
+    for i=1:length(UnitCF)
+        loglog(UnitCF(i),UnitQ10(i),[colors{i} 'x']); hold on;
+    end
+    loglog(QlowM97(:,1),QlowM97(:,2),'k-');
+    loglog(QhighM97(:,1),QhighM97(:,2),'k-');
+    hold off;
+    ylabel('Q_{10}');  xlabel('Best Frequency (kHz)');
+    axis([0.1 20 0.3 20]);
+
+    if exist('Rate_failpoint')
+        figure,
+        subplot(411), hold on;
+        for i=1:length(UnitCF)
+            plot(UnitCF(i),Rate_failpoint(i),[colors{i} 'o']);
+        end
+        for i=1:length(Rate_fail_limit)
+            if Rate_fail_limit(i)>0
+                text(UnitCF(i),Rate_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
+            elseif Rate_fail_limit(i)<0
+                text(UnitCF(i),Rate_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
+            end
+        end
+        hold off;
+        axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
+        xlabel('CF');
+        ylabel('SNR (dB)');
+        title('Rate Failure Point');
+
+        subplot(412), hold on;
+        for i=1:length(UnitCF)
+            plot(UnitCF(i),ALSR_failpoint(i),[colors{i} 'o']);
+        end
+        for i=1:length(ALSR_fail_limit)
+            if ALSR_fail_limit(i)>0
+                text(UnitCF(i),ALSR_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
+            elseif ALSR_fail_limit(i)<0
+                text(UnitCF(i),ALSR_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
+            end
+        end
+        hold off;
+        axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
+        xlabel('CF');
+        ylabel('SNR (dB)');
+        title('ALSR Failure Point');
+
+        subplot(413), hold on;
+        for i=1:length(UnitCF)
+            hLine=plot(UnitCF(i),Nscc_CD_pos_failpoint(i),[colors{i} 'o']);
+            if i>1
+                set(get(get(hLine,'Annotation'),'LegendInformation'),...
+                    'IconDisplayStyle','off'); % Exclude line from legend
+            end
+        end
+        for i=1:length(Nscc_CD_pos_fail_limit)
+            if Nscc_CD_pos_fail_limit(i)>0
+                text(UnitCF(i),Nscc_CD_pos_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
+            elseif Nscc_CD_pos_fail_limit(i)<0
+                text(UnitCF(i),Nscc_CD_pos_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
+            end
+        end
+        for i=1:length(UnitCF)
+            hLine=plot(UnitCF(i),Nscc_CD_neg_failpoint(i),[colors{i} 'o'],'MarkerFaceColor',colors{i});
+            if i>1
+                set(get(get(hLine,'Annotation'),'LegendInformation'),...
+                    'IconDisplayStyle','off'); % Exclude line from legend
+            end
+        end
+        for i=1:length(Nscc_CD_neg_fail_limit)
+            if Nscc_CD_neg_fail_limit(i)>0
+                text(UnitCF(i),Nscc_CD_neg_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
+            elseif Nscc_CD_neg_fail_limit(i)<0
+                text(UnitCF(i),Nscc_CD_neg_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
+            end
+        end
+        hold off;
+        axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
+        legend('+ slope','- slope');
+        xlabel('CF');
+        ylabel('SNR (dB)');
+        title('Correlation Failure Point');
+
+        subplot(414), hold on;
+        for i=1:length(UnitCF)
+            hLine=plot(UnitCF(i),Nscc0_pos_failpoint(i),[colors{i} 'o']);
+            if i>1
+                set(get(get(hLine,'Annotation'),'LegendInformation'),...
+                    'IconDisplayStyle','off'); % Exclude line from legend
+            end
+        end
+        for i=1:length(Nscc0_pos_fail_limit)
+            if Nscc0_pos_fail_limit(i)>0
+                text(UnitCF(i),Nscc0_pos_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
+            elseif Nscc0_pos_fail_limit(i)<0
+                text(UnitCF(i),Nscc0_pos_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
+            end
+        end
+        for i=1:length(UnitCF)
+            hLine=plot(UnitCF(i),Nscc0_neg_failpoint(i),[colors{i} 'o'],'MarkerFaceColor',colors{i});
+            if i>1
+                set(get(get(hLine,'Annotation'),'LegendInformation'),...
+                    'IconDisplayStyle','off'); % Exclude line from legend
+            end
+        end
+        for i=1:length(Nscc0_neg_fail_limit)
+            if Nscc0_neg_fail_limit(i)>0
+                text(UnitCF(i),Nscc0_neg_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
+            elseif Nscc0_neg_fail_limit(i)<0
+                text(UnitCF(i),Nscc0_neg_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
+            end
+        end
+        hold off;
+        axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
+        legend('+ slope','- slope');
+        xlabel('CF');
+        ylabel('SNR (dB)');
+        title('Delay Failure Point');
+    end
 end
-hold off;
-legend('Miller et al. (1997)','This Experiment');
-ylabel('Threshold (dB SPL)'); xlabel('Best Frequency (kHz)');
-axis([0.1 20 -10 110]);
-% Plot Q10
-subplot(212),
-QlowM97(:,1)=[.27 10]';
-QhighM97(:,1)=QlowM97(:,1); % Add lines from Miller et al 1997 Normal data
-QlowM97(:,2)=10^.2779*QlowM97(:,1).^.4708;
-QhighM97(:,2)=10^.6474*QlowM97(:,1).^.4708; % 5th and 95th percentiles from Miller et al, 1997
-for i=1:length(UnitCF)
-    loglog(UnitCF(i),UnitQ10(i),[colors{i} 'x']); hold on;
-end
-loglog(QlowM97(:,1),QlowM97(:,2),'k-');
-loglog(QhighM97(:,1),QhighM97(:,2),'k-');
-hold off;
-ylabel('Q_{10}');  xlabel('Best Frequency (kHz)');
-axis([0.1 20 0.3 20]);
-
-if exist('Rate_failpoint')
-    figure,
-    subplot(411), hold on;
-    for i=1:length(UnitCF)
-        plot(UnitCF(i),Rate_failpoint(i),[colors{i} 'o']);
-    end
-    for i=1:length(Rate_fail_limit)
-        if Rate_fail_limit(i)>0
-            text(UnitCF(i),Rate_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
-        elseif Rate_fail_limit(i)<0
-            text(UnitCF(i),Rate_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
-        end
-    end
-    hold off;
-    axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
-    xlabel('CF');
-    ylabel('SNR (dB)');
-    title('Rate Failure Point');
-
-    subplot(412), hold on;
-    for i=1:length(UnitCF)
-        plot(UnitCF(i),ALSR_failpoint(i),[colors{i} 'o']);
-    end 
-    for i=1:length(ALSR_fail_limit)
-        if ALSR_fail_limit(i)>0
-            text(UnitCF(i),ALSR_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
-        elseif ALSR_fail_limit(i)<0
-            text(UnitCF(i),ALSR_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
-        end
-    end
-    hold off;
-    axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
-    xlabel('CF');
-    ylabel('SNR (dB)');
-    title('ALSR Failure Point');
-
-    subplot(413), hold on;
-    for i=1:length(UnitCF)
-        hLine=plot(UnitCF(i),Nscc_CD_pos_failpoint(i),[colors{i} 'o']);
-        if i>1
-            set(get(get(hLine,'Annotation'),'LegendInformation'),...
-                'IconDisplayStyle','off'); % Exclude line from legend
-        end
-    end 
-    for i=1:length(Nscc_CD_pos_fail_limit)
-        if Nscc_CD_pos_fail_limit(i)>0
-            text(UnitCF(i),Nscc_CD_pos_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
-        elseif Nscc_CD_pos_fail_limit(i)<0
-            text(UnitCF(i),Nscc_CD_pos_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
-        end
-    end
-    for i=1:length(UnitCF)
-        hLine=plot(UnitCF(i),Nscc_CD_neg_failpoint(i),[colors{i} 'o'],'MarkerFaceColor',colors{i});
-        if i>1
-            set(get(get(hLine,'Annotation'),'LegendInformation'),...
-                'IconDisplayStyle','off'); % Exclude line from legend
-        end
-    end 
-    for i=1:length(Nscc_CD_neg_fail_limit)
-        if Nscc_CD_neg_fail_limit(i)>0
-            text(UnitCF(i),Nscc_CD_neg_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
-        elseif Nscc_CD_neg_fail_limit(i)<0
-            text(UnitCF(i),Nscc_CD_neg_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
-        end
-    end
-    hold off;
-    axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
-    legend('+ slope','- slope');
-    xlabel('CF');
-    ylabel('SNR (dB)');
-    title('Correlation Failure Point');
-
-    subplot(414), hold on;
-    for i=1:length(UnitCF)
-        hLine=plot(UnitCF(i),Nscc0_pos_failpoint(i),[colors{i} 'o']);
-        if i>1
-            set(get(get(hLine,'Annotation'),'LegendInformation'),...
-                'IconDisplayStyle','off'); % Exclude line from legend
-        end
-    end 
-    for i=1:length(Nscc0_pos_fail_limit)
-        if Nscc0_pos_fail_limit(i)>0
-            text(UnitCF(i),Nscc0_pos_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
-        elseif Nscc0_pos_fail_limit(i)<0
-            text(UnitCF(i),Nscc0_pos_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
-        end
-    end
-    for i=1:length(UnitCF)
-        hLine=plot(UnitCF(i),Nscc0_neg_failpoint(i),[colors{i} 'o'],'MarkerFaceColor',colors{i});
-        if i>1
-            set(get(get(hLine,'Annotation'),'LegendInformation'),...
-                'IconDisplayStyle','off'); % Exclude line from legend
-        end
-    end 
-    for i=1:length(Nscc0_neg_fail_limit)
-        if Nscc0_neg_fail_limit(i)>0
-            text(UnitCF(i),Nscc0_neg_failpoint(i),'\uparrow','VerticalAlign','bottom','HorizontalAlign','center','color',colors{i});
-        elseif Nscc0_neg_fail_limit(i)<0
-            text(UnitCF(i),Nscc0_neg_failpoint(i),'\downarrow','VerticalAlign','top','HorizontalAlign','center','color',colors{i});
-        end
-    end
-    hold off;
-    axis([0.8*min(UnitCF) 1.2*max(UnitCF) -30 30]);
-    legend('+ slope','- slope');
-    xlabel('CF');
-    ylabel('SNR (dB)');
-    title('Delay Failure Point');
-end
-
 
