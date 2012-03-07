b35e60b663c2c025f56afee1ddd097c0055352c5
Jon2980 <Jon2980@daba21ff-bcea-44df-ba80-775e93682a96>
2012-03-07 18:35:00 +0000

    fixed compatibility issues with older versions of Matlab (now works with Matlab 6.1)

--- NEL/Users/JB/Templates/TwoBandGain.m
+++ NEL/Users/JB/Templates/TwoBandGain.m
@@ -2,14 +2,14 @@ function out = TwoBandGain(in,fc,fs,gains)
 
 % design 8th-order Linkwitz-Riley crossover
 [b,a] = butter(4,fc/(fs/2),'low');  % low-pass
-Hd_lp = dfilt.df2(b,a);
-Hd_lp = dfilt.cascade(Hd_lp,Hd_lp);
+out_lo=filter(b,a,in);
+out_lo=filter(b,a,out_lo);
 
 [b,a] = butter(4,fc/(fs/2),'high');  % high-pass
-Hd_hp = dfilt.df2(b,a);
-Hd_hp = dfilt.cascade(Hd_hp,Hd_hp);
+out_hi=filter(b,a,in);
+out_hi=filter(b,a,out_hi);
 
-out = 10^(gains(1)/20)*filter(Hd_lp,in) + ...
-    10^(gains(2)/20)*filter(Hd_hp,in);
-end %function
+out = 10^(gains(1)/20)*out_lo + ...
+    10^(gains(2)/20)*out_hi;
+
 
