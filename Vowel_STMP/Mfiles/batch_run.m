7b7120b7c6be4f46e3fca8160a28930dbafd7745
Jon Boley <JonBoley@users.noreply.github.com>
2012-03-21 16:43:03 +0000

    Added synchrony plots, changed CD units, hardcoded loadBOOL, and added a few units to the batch file

--- Vowel_STMP/Mfiles/batch_run.m
+++ Vowel_STMP/Mfiles/batch_run.m
@@ -116,34 +116,37 @@ end
 dates=repmat({date},size(unitNums));
 
 %% Run a combo of experiments
-% dates = {'041811','041811','041811','041811','041811','041811',...
-%     '062711','062711','062711','062711','062711','062711',...
-%     '062311','062311','062311','062311',...
-%     '072111','072111','072111','072111','072111','072111',...
-%     '080111','080111','080111','080111','080111',...
-%     '080911','080911','080911','080911','080911','080911','080911','080911'...
-%     };
-% unitNums = {1.03,2.01,2.02,2.07,3.03,3.04,...
-%     1.01,1.02,1.03,1.04,2.01,2.03,...
-%     1.01,1.09,1.11,1.14,...
-%     1.01, 1.06, 1.11, 1.12, 1.15, 1.16,...
-%     2.02, 4.01, 4.02, 4.03, 4.05,...
-%     1.04, 1.05, 1.06, 1.07, 1.09, 1.11, 1.15, 1.16...
-%     };
-% colors = {'k','k','k','k','k','k',...
-%     'k','k','k','k','k','k',...
-%     'r','r','r','r',...
-%     'r','r','r','r','r','r',...
-%     'r','r','r','r','r',...
-%     'r','r','r','r','r','r','r','r'...
-%     };
+dates = {'041811','041811','041811','041811','041811','041811',...
+    '062711','062711','062711','062711','062711','062711',...
+    '072011','072011',...
+    '062311','062311','062311','062311',...
+    '072111','072111','072111','072111','072111','072111',...
+    '080111','080111','080111','080111','080111',...
+    '080911','080911','080911','080911','080911','080911','080911','080911'...
+    };
+unitNums = {1.03,2.01,2.02,2.07,3.03,3.04,...
+    1.01,1.02,1.03,1.04,2.01,2.03,...
+    1.02,1.03,...
+    1.01,1.09,1.11,1.14,...
+    1.01, 1.06, 1.11, 1.12, 1.15, 1.16,...
+    2.02, 4.01, 4.02, 4.03, 4.05,...
+    1.04, 1.05, 1.06, 1.07, 1.09, 1.11, 1.15, 1.16...
+    };
+colors = {'k','k','k','k','k','k',...
+    'k','k','k','k','k','k',...
+    'k','k',...
+    'r','r','r','r',...
+    'r','r','r','r','r','r',...
+    'r','r','r','r','r',...
+    'r','r','r','r','r','r','r','r'...
+    };
 
 %% The loop
 RecalcAll = 0;  % if enabled, this automatically recalculates everything,
                 % but loads previous characteristic delays
 for i=1:length(unitNums)
     date = dates{i};
-    disp(sprintf('%d) [%s] Calculating unit %1.2f...',i,date,unitNums{i}));
+    disp(sprintf('%d of %d) [%s] Calculating unit %1.2f...',i,length(unitNums),date,unitNums{i}));
 
     % Use this for SNR conditions:
         [UnitCF(i),UnitThresh(i),UnitQ10(i),...
@@ -153,36 +156,195 @@ for i=1:length(unitNums)
             Nscc_CD_neg_failpoint(i),Nscc_CD_neg_fail_limit(i),...
             Nscc0_pos_failpoint(i),Nscc0_pos_fail_limit(i),...
             Nscc0_neg_failpoint(i),Nscc0_neg_fail_limit(i),...
-            Rho_width{i},CD_slope{i},SNR{i}]=...
+            Rho_width{i},CD_slope{i},SNR{i},...
+            SyncValues{i},FeatureFreqs{i}]=...
     UnitLook_EHIN_CoincDet2_JB(date,num2str(unitNums{i}),RecalcAll);
 
     % Use this for level conditions:
     %     [UnitCF(i),UnitThresh(i),UnitQ10(i)]=...
     %         UnitLook_EH_CoincDet2_JB(num2str(unitNums{i}));
 
-    if ~RecalcAll, pause; end
+%     if ~RecalcAll, pause; end
     close all; home;
 end
 
-
-if exist('Rho_width')
-    figure, subplot(211), hold on;
-    for i=1:length(Rho_width)
-        plot(SNR{i},cell2mat(Rho_width{i}(1,:)),colors{i});
+if exist('SyncValues') % plot SyncValues as a function of CF & noise level
+    % SyncValues{featureNum,snrNum} = [FeatureFreqs;BFs;Synchs];
+    figure; 
+    FeatureNums=[1 3 5]; %F0,F1,F2
+    for i=1:length(SyncValues) % for each unit
+        plotNum=0;
+        for FeatureNum=FeatureNums % sync to which feature?
+            for j=1:length(SNR{i})
+                plotNum=plotNum+1;
+                subplot(length(FeatureNums),length(SNR{i}),plotNum), hold on;
+                switch j
+                    case 1
+                        indx_snr = find(SNR{i}==max(SNR{i}));
+                    case 2
+                        indx_snr = find(SNR{i}==0);
+                    case 3
+                        indx_snr = find(SNR{i}~=max(SNR{i}) & SNR{i}~=0);
+                end
+                
+                syncFreqsF1 = unique(SyncValues{i}{1,indx_snr}(1,:)); %frequencies for which sync was calculated
+                syncFreqsT1 = unique(SyncValues{i}{2,indx_snr}(1,:));
+                syncFreqsF2 = unique(SyncValues{i}{3,indx_snr}(1,:));
+                syncFreqsT2 = unique(SyncValues{i}{4,indx_snr}(1,:));
+                
+                indx_f1 = interp1(syncFreqsF1,1:length(syncFreqsF1),...
+                    FeatureFreqs{i}{1,indx_snr}(FeatureNum),'nearest','extrap');
+                indx_t1 = interp1(syncFreqsT1,1:length(syncFreqsT1),...
+                    FeatureFreqs{i}{2,indx_snr}(FeatureNum),'nearest','extrap');
+                indx_f2 = interp1(syncFreqsF2,1:length(syncFreqsF2),...
+                    FeatureFreqs{i}{3,indx_snr}(FeatureNum),'nearest','extrap');
+                indx_t2 = interp1(syncFreqsT2,1:length(syncFreqsT2),...
+                    FeatureFreqs{i}{4,indx_snr}(FeatureNum),'nearest','extrap');
+                
+                syncPlotCFsF1 = SyncValues{i}{1,indx_snr}(2,...
+                    SyncValues{i}{1,indx_snr}(1,:)==syncFreqsF1(indx_f1));
+                syncPlotCFsT1 = SyncValues{i}{2,indx_snr}(2,...
+                    SyncValues{i}{2,indx_snr}(1,:)==syncFreqsT1(indx_t1));
+                syncPlotCFsF2 = SyncValues{i}{3,indx_snr}(2,...
+                    SyncValues{i}{3,indx_snr}(1,:)==syncFreqsF2(indx_f2));
+                syncPlotCFsT2 = SyncValues{i}{4,indx_snr}(2,...
+                    SyncValues{i}{4,indx_snr}(1,:)==syncFreqsT2(indx_t2));
+                
+                syncPlotValsF1 = SyncValues{i}{1,indx_snr}(3,...
+                    SyncValues{i}{1,indx_snr}(1,:)==syncFreqsF1(indx_f1));
+                syncPlotValsT1 = SyncValues{i}{2,indx_snr}(3,...
+                    SyncValues{i}{2,indx_snr}(1,:)==syncFreqsT1(indx_t1));
+                syncPlotValsF2 = SyncValues{i}{3,indx_snr}(3,...
+                    SyncValues{i}{3,indx_snr}(1,:)==syncFreqsF2(indx_f2));
+                syncPlotValsT2 = SyncValues{i}{4,indx_snr}(3,...
+                    SyncValues{i}{4,indx_snr}(1,:)==syncFreqsT2(indx_t2));
+                
+                % We'll just plot sync @ CF for now
+                indxCF_f1 = interp1(syncPlotCFsF1,1:length(syncPlotCFsF1),UnitCF(i),'nearest','extrap');
+                indxCF_t1 = interp1(syncPlotCFsT1,1:length(syncPlotCFsT1),UnitCF(i),'nearest','extrap');
+                indxCF_f2 = interp1(syncPlotCFsF2,1:length(syncPlotCFsF2),UnitCF(i),'nearest','extrap');
+                indxCF_t2 = interp1(syncPlotCFsT2,1:length(syncPlotCFsT2),UnitCF(i),'nearest','extrap');
+                
+                hold on;
+                h1=semilogx(UnitCF(i),syncPlotValsF1(indxCF_f1),[colors{i},'x']);
+                if isnan(syncPlotValsF1(indxCF_f1))
+                    switch colors{i}
+                        case 'k', h1=semilogx(UnitCF(i),0,[colors{i},'x']); 
+                        otherwise, h1=semilogx(UnitCF(i),0.01,[colors{i},'x']);
+                    end
+                end
+                
+%                 h2=semilogx(UnitCF(i),syncPlotValsT1(indxCF_t1),[colors{i},'s']);
+%                 if isnan(syncPlotValsT1(indxCF_t1))
+%                     switch colors{i}
+%                         case 'k', h2=semilogx(UnitCF(i),0,[colors{i},'s']); 
+%                         otherwise, h2=semilogx(UnitCF(i),0.01,[colors{i},'s']);
+%                     end
+%                 end
+                
+                h3=semilogx(UnitCF(i),syncPlotValsF2(indxCF_f2),[colors{i},'o']);
+                if isnan(syncPlotValsF2(indxCF_f2))
+                    switch colors{i}
+                        case 'k', h3=semilogx(UnitCF(i),0,[colors{i},'o']); 
+                        otherwise, h3=semilogx(UnitCF(i),0.01,[colors{i},'o']);
+                    end
+                end
+                
+%                 h4=semilogx(UnitCF(i),syncPlotValsT2(indxCF_t2),[colors{i},'h']); hold off;
+%                 if isnan(syncPlotValsT2(indxCF_t2))
+%                     switch colors{i}
+%                         case 'k', h4=semilogx(UnitCF(i),0,[colors{i},'h']); 
+%                         otherwise, h4=semilogx(UnitCF(i),0.01,[colors{i},'h']);
+%                     end
+%                 end
+                
+                set(gca,'XScale','log'); set(gca,'XTick',[1 2 4]);
+                xlim([0.35 7]); ylim([0 1]);
+                
+                if j==1 % apply ylabels
+                    switch FeatureNum
+                        case 1
+                            ylabel('Sync (F0)');
+                        case 3
+                            ylabel('Sync (F1)');
+                        case 5
+                            ylabel('Sync (F2)');
+                    end
+                end
+                switch FeatureNum % apply xlabels & titles
+                    case FeatureNums(1)
+                        switch j
+                            case 1
+                                title('In Quiet');
+                            case 2
+                                title('Equal SPL');
+                            case 3
+                                title('Equal SL');
+%                                 legend([h1 h2 h3 h4],'F1 @ CF','T1 @ CF','F2 @ CF','T2 @ CF');
+                                legend([h1 h3],'F1 @ CF','F2 @ CF');
+%                                 legend([h2 h4],'T1 @ CF','T2 @ CF');
+                        end
+                    case FeatureNums(end)
+                        xlabel('CF (kHz)');
+                end %switch FeatureNum
+            end %for i=1:length(SNR{i})
+        end %for FeatureNum=FeatureNums
     end
-    hold off; set(gcf,'XDir','reverse');
-    
-    subplot(212), hold on;
-    for i=1:length(CD_slope)
-        plot(SNR{i},cell2mat(CD_slope{i}(1,:)),colors{i});
-    end
-    hold off; set(gcf,'XDir','reverse');
 end
 
 
 
-if exist('UnitCF')
-    % Plot Threshold
+if exist('Rho_width') % plot Rho & CD as a function of CF & noise level
+    figure;
+    FeatureNum=3;
+    for i=1:length(Rho_width)
+        subplot(231), hold on;
+        indx = find(SNR{i}==max(SNR{i}));
+        semilogx(UnitCF(i),cell2mat(Rho_width{i}(FeatureNum,indx)),[colors{i},'o']);
+        
+        subplot(232), hold on;
+        indx = find(SNR{i}==0);
+        semilogx(UnitCF(i),cell2mat(Rho_width{i}(FeatureNum,indx)),[colors{i},'o']);
+        
+        subplot(233), hold on;
+        indx = find(SNR{i}~=max(SNR{i}) & SNR{i}~=0);
+        semilogx(UnitCF(i),cell2mat(Rho_width{i}(FeatureNum,indx)),[colors{i},'o']);
+    end
+    subplot(231), set(gca,'XScale','log'); set(gca,'XTick',[1 2 4]);
+        title('In Quiet');
+    ylabel('\Delta\rho@0.8 (octaves)');
+    subplot(232), set(gca,'XScale','log'); set(gca,'XTick',[1 2 4]);
+        title('Equal SPL');
+    subplot(233), set(gca,'XScale','log'); set(gca,'XTick',[1 2 4]);
+        title('Equal SL');
+    
+    
+    for i=1:length(CD_slope)
+        subplot(234), hold on;
+        indx = find(SNR{i}==max(SNR{i}));
+        semilogx(UnitCF(i),(CD_slope{i}(FeatureNum,indx)),[colors{i},'o']);
+        
+        subplot(235), hold on;
+        indx = find(SNR{i}==0);
+        semilogx(UnitCF(i),(CD_slope{i}(FeatureNum,indx)),[colors{i},'o']);
+        
+        subplot(236), hold on;
+        indx = find(SNR{i}~=max(SNR{i}) & SNR{i}~=0);
+        semilogx(UnitCF(i),(CD_slope{i}(FeatureNum,indx)),[colors{i},'o']);
+    end
+    subplot(234), set(gca,'XScale','log'); set(gca,'XTick',[1 2 4]);
+        xlabel('CF (kHz)');
+    ylabel('CD Slope (CF cycles/octave)');
+    subplot(235), set(gca,'XScale','log'); set(gca,'XTick',[1 2 4]);
+        xlabel('CF (kHz)');
+    subplot(236), set(gca,'XScale','log'); set(gca,'XTick',[1 2 4]);
+        xlabel('CF (kHz)');
+      
+end
+
+
+
+if exist('UnitCF') % Plot Thresholds/Q10s & Failpoints
     figure, subplot(211),
     load normema % load normal thresholds
     semilogx(normt(1,:),normt(2,:),'k'); hold on;
