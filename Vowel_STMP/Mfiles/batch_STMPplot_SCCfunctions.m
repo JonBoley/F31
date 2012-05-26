bcffce2aa2d5e76411b682bd9e6c309588a15b22
Jon2980 <Jon2980@daba21ff-bcea-44df-ba80-775e93682a96>
2012-05-26 20:11:49 +0000

    mostly changes to how CD is calculated

--- Vowel_STMP/Mfiles/batch_STMPplot_SCCfunctions.m
+++ Vowel_STMP/Mfiles/batch_STMPplot_SCCfunctions.m
@@ -14,18 +14,19 @@ unitNums = {1.01,1.09,1.11,1.14,...
     1.04, 1.05, 1.06, 1.07, 1.09, 1.11, 1.15, 1.16...
     };
 
-for i=1%:length(dates)
-    FeatureIndices=3;
-    AttenIndices=1;
-    
-    % just plot
-%     STMPplot_SCCfunctions(dates{i},num2str(unitNums{i}),FeatureIndices,AttenIndices);
-    
-    % interactive plot (lets you manually fix peaks)
-    if exist('h_calcCDreBF_manual','var'), uiwait(h_calcCDreBF_manual); end; 
-    h_calcCDreBF_manual = calcCDreBF_manual(dates{i},num2str(unitNums{i}),FeatureIndices,AttenIndices);
-    
-%     count=fprintf('%d of %d',i,length(dates));
-%     pause; fprintf(repmat('\b',1,count));
+for i=1:length(dates)
+    count=fprintf('%d of %d',i,length(dates));
+    for FeatureIndices=1:4
+        for AttenIndices=1:3
+            % just plot
+%             STMPplot_SCCfunctions(dates{i},num2str(unitNums{i}),FeatureIndices,AttenIndices);
+
+            % interactive plot (lets you manually fix peaks)
+            calcCDreBF_manual({dates{i}},num2str(unitNums{i}),FeatureIndices,AttenIndices);
+        end
+    end
+
+%     pause;
+    fprintf(repmat('\b',1,count));
 end
-clear h_calcCDreBF_manual;
+disp('Done!');
