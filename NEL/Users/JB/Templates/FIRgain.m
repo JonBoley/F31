82f9f0901bbc7cb065ab74394c4a049db7ddcd37
Jon2980 <Jon2980@daba21ff-bcea-44df-ba80-775e93682a96>
2012-07-29 01:10:52 +0000

    fixed strategy index & limited FIRgain to Nyquist

--- NEL/Users/JB/Templates/FIRgain.m
+++ NEL/Users/JB/Templates/FIRgain.m
@@ -2,9 +2,18 @@ function out = FIRgain(in,gain_db,f_hz,Fs)
 f = f_hz/(Fs/2);
 a = 10.^(gain_db/20);
 
-%create freq pairs
-f=[f(1:end-1) f(end-1)+eps f(end) f(end)+eps 1];
-a=[a(1:end-1) a(end-1) a(end) a(end) 0];
+% make sure we're less than Nyquist
+f=f(f<1);
+a=a(f<1);
+
+if mod(length(f),2) 
+    %create freq pairs
+    f=[f(1:end-1) f(end-1)+eps f(end) f(end)+eps 1];
+    a=[a(1:end-1) a(end-1) a(end) a(end) 0];
+else
+    f=[f f(end)+eps 1];
+    a=[a a(end) 0];
+end
 
 % design 32-order filter
 b = firpm(32,f,a);
